c4947271c19957afd6f685e8a06e54ddcb5c93d0
quynv-1217 <nguyen.van.quy-b@sun-asterisk.com>
2023-03-06 15:32:32 +0700

    feat: change GA4 config organization-feature

---
+++ organization-feature/organization-feature/2e3019c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{314:function(n,l,o){"use strict";o.r(l);var e=o(313).default,t=o(6),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
